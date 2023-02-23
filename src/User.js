1c7aff7058c1318c5d4b8f348863e7f9b7beeb78
Galina S <98215274+Galina-S@users.noreply.github.com>
2023-02-23 14:39:34 +0100

    Create User.js

---
+++ src/User.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+	username: String,
+	email: String,
+	hash: String,
+});
+
+export const User = mongoose.model("user", userSchema)
