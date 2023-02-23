9e90ccf04613d656da161fc72221a54fe74e4464
Galina S <98215274+Galina-S@users.noreply.github.com>
2023-02-23 14:40:01 +0100

    Update and rename src/User.js to users.json

--- src/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-	username: String,
-	email: String,
-	hash: String,
-});
-
-export const User = mongoose.model("user", userSchema)
